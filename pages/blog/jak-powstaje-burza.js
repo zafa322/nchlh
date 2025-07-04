610499f1810bea39e8196aeb09c98f9da9ffa2b4
zafa322 <145940698+zafa322@users.noreply.github.com>
2025-07-04 23:21:38 +0100

    Update jak-powstaje-burza.js

--- pages/blog/jak-powstaje-burza.js
+++ pages/blog/jak-powstaje-burza.js
@@ -1,30 +1,32 @@
-import Head from 'next/head';
+import Head from 'next/head'
 
-export default function Article() {
+export default function JakPowstajeBurza() {
   return (
-    <div>
+    <>
       <Head>
         <title>Jak powstaje burza? Poznaj etapy tworzenia burz i ich skutki.</title>
         <meta name="description" content="Jak powstaje burza? Poznaj etapy tworzenia burz i ich skutki." />
         <script
           type="application/ld+json"
-          dangerouslySetInnerHTML={
+          dangerouslySetInnerHTML={{
             __html: JSON.stringify({
               "@context": "https://schema.org",
               "@type": "Article",
               "headline": "Jak powstaje burza? Poznaj etapy tworzenia burz i ich skutki.",
               "inLanguage": "pl",
-              "author": { "@type": "Person", "name": "Redakcja Burza" },
-              "publisher": { "@type": "Organization", "name": "Burza Info" },
+              "author": {
+                "@type": "Organization",
+                "name": "Radar Burzowy"
+              },
               "datePublished": "2025-07-04"
             })
-          }
+          }}
         />
       </Head>
       <main>
-        <h1>Jak powstaje burza? Poznaj etapy tworzenia burz i ich skutki.</h1>
-        <p>To jest przykładowy artykuł zoptymalizowany pod kątem SEO.</p>
+        <h1>Jak powstaje burza?</h1>
+        <p>Poznaj etapy tworzenia burzy i skutki atmosferyczne.</p>
       </main>
-    </div>
-  );
+    </>
+  )
 }
