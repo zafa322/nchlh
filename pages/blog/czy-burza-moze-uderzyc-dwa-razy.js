fddf58b832d973cf3e39c2ee37513fe99f14b095
zafa322 <145940698+zafa322@users.noreply.github.com>
2025-07-04 23:20:49 +0100

    Update czy-burza-moze-uderzyc-dwa-razy.js

--- pages/blog/czy-burza-moze-uderzyc-dwa-razy.js
+++ pages/blog/czy-burza-moze-uderzyc-dwa-razy.js
@@ -1,30 +1,32 @@
-import Head from 'next/head';
+import Head from 'next/head'
 
-export default function Article() {
+export default function CzyBurzaMozeUderzycDwaRazy() {
   return (
-    <div>
+    <>
       <Head>
         <title>Czy burza może uderzyć dwa razy w to samo miejsce?</title>
         <meta name="description" content="Czy burza może uderzyć dwa razy w to samo miejsce?" />
         <script
           type="application/ld+json"
-          dangerouslySetInnerHTML={
+          dangerouslySetInnerHTML={{
             __html: JSON.stringify({
               "@context": "https://schema.org",
               "@type": "Article",
               "headline": "Czy burza może uderzyć dwa razy w to samo miejsce?",
               "inLanguage": "pl",
-              "author": { "@type": "Person", "name": "Redakcja Burza" },
-              "publisher": { "@type": "Organization", "name": "Burza Info" },
+              "author": {
+                "@type": "Organization",
+                "name": "Radar Burzowy"
+              },
               "datePublished": "2025-07-04"
             })
-          }
+          }}
         />
       </Head>
       <main>
-        <h1>Czy burza może uderzyć dwa razy w to samo miejsce?</h1>
-        <p>To jest przykładowy artykuł zoptymalizowany pod kątem SEO.</p>
+        <h1>Czy burza może uderzyć dwa razy?</h1>
+        <p>Odpowiedź na często zadawane pytanie o burze.</p>
       </main>
-    </div>
-  );
+    </>
+  )
 }
